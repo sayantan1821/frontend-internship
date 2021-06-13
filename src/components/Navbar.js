3bf0f3bc22fb8135c36876035177fde458d659ce
Sayantan Kapat <ksayantan1823@gmail.com>
2021-06-13 23:49:28 +0530

    initialize the project

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BiRocket } from "react-icons/bi";
+
+
+function Navbar() {
+    return (
+        <div>
+            <BiRocket />
+        </div>
+    )
+}
+
+export default Navbar;
